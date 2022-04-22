71eefeacc71b1a8999eb4611b7e7adc0f1974199
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-22 13:57:19 -0300

    feat: implemented readOne UserModel and Login type in types.

--- project/src/models/MongoModel.ts
+++ project/src/models/MongoModel.ts
@@ -1,10 +1,14 @@
 import { Model as M, Document } from 'mongoose';
 import { Model } from '../interfaces/ModelInterface';
+import { Login } from '../types';
 
 abstract class MongoModel<T> implements Model<T> {
   constructor(public model: M<T & Document>) {}
 
-  create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+  create = async (obj: T): Promise<T> => this.model.create(obj);
+
+  readOne = async (obj: T | Login):
+  Promise<T | null> => this.model.findOne(obj);
 }
 
 export default MongoModel;
---
+++ project/src/types/index.ts
@@ -0,0 +1,8 @@
+export type Login = {
+  email: string,
+};
+
+export type TokenType = {
+  id: string;
+  email: string;
+};

e50fc9fb7edc7fb4fc4053c61179c6af3ec3b285
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-22 12:33:29 -0300

    feat: Implemented abstract class Service

--- project/src/interfaces/RequestsInterfaces.ts
+++ project/src/interfaces/RequestsInterfaces.ts
@@ -3,7 +3,3 @@ import { Request } from 'express';
 export interface RequestWithBody<T> extends Request {
   body: T;
 }
-
-export interface RequestWithParams extends Request {
-  params: { id: string };
-}

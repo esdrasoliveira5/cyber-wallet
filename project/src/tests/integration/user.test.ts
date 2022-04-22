0fcd917d70fe2b34904740dc1a657ef0d22b4ab0
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-22 13:35:14 -0300

    test: add integration test POST user/login

--- project/src/tests/integration/user.test.ts
+++ project/src/tests/integration/user.test.ts
@@ -124,3 +124,76 @@ describe('1 - Test endpoint POST /user', () => {
     });
   });
 });
+
+describe('2 - Test endpoint POST /user/login', () => {
+  describe('2.1 - if success', () => {
+    let chaiHttpResponse: Response;
+
+    before(() => {
+      sinon
+      .stub(user.model, 'findOne')
+      .resolves(payload);
+    });
+    after(()=>{
+      sinon.restore();
+    });
+    it('a) return status 201 and the user created', async () => {
+      chaiHttpResponse = await chai
+         .request(server.app)
+         .post('/user/login')
+         .set('X-API-Key', 'foobar')
+         .send({
+          "email": "roberto@email.com",
+          "password": "roberto_password",
+        });
+      expect(chaiHttpResponse).to.have.status(201);
+      expect(chaiHttpResponse.body).to.deep.equal({
+        "user": {
+          "_id": "6260bca97c58e5a0b7847cfa",
+          "name": "Roberto",
+          "lastName": "Oliveira",
+          "email": "roberto@email.com",
+          "contact": "+5511987654321",
+          "password": "$2b$10$JOmGDGptDGC1.eLa3OMj0uAk4FxZT2SjLH0lbP3Uh9W7iDHGN3Lp6",
+          "balance": 0,
+          "transactions": [],
+          "address": {
+              "street": "avenida",
+              "number": "100A",
+              "district": "Bairro",
+              "zipcode": "45687-899",
+              "city": "cidade",
+              "state": "estado",
+              "country": "pais"
+          }
+        },
+        "token": "bearer Token"
+      });
+    });
+  });
+  describe('2.2 - if fail', () => {
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon
+      .stub(user.model, 'findOne')
+      .rejects({ message: 'Internal Server Error'});
+      sinon
+    });
+    after(()=>{
+      sinon.restore();
+    });
+
+    it('a) return status 500 and the error message "Internal Server Error"', async () => {
+      chaiHttpResponse = await chai
+         .request(server.app)
+         .post('/user/login')
+         .set('X-API-Key', 'foobar')
+         .send({
+          "email": "roberto@email.com",
+          "password": "roberto_password",
+      });
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body).to.deep.equal({ "error": "Erro: Internal Server Error"});
+    });
+  });
+});

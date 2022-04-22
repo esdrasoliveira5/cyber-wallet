f917dbe269f37088e91e5c2443f2de0d3dfd80a2
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-22 10:06:17 -0300

    test: add integration test POST /user

---
+++ project/src/tests/integration/user.test.ts
@@ -0,0 +1,129 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { Response } from 'superagent';
+import UserModel from '../../models/UserModel';
+import server from '../../server';
+const user = new UserModel();
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const payload = {
+  _id: '6260bca97c58e5a0b7847cfa',
+  name: 'Roberto',
+  lastName: 'Oliveira',
+  email: 'roberto@email.com',
+  contact: '+5511987654321',
+  password: '123456789',
+  balance: '',
+  transactions: [],
+  address: {
+    street: 'avenida',
+    number: '100A',
+    district: 'Bairro',
+    zipcode: '45687-899',
+    city: 'cidade',
+    state: 'estado',
+    country: 'pais'
+  }
+}
+
+describe('1 - Test endpoint POST /user', () => {
+  describe('1.1 - if success', () => {
+    let chaiHttpResponse: Response;
+
+    before(() => {
+      sinon
+      .stub(user.model, 'findOne')
+      .resolves(null);
+      sinon
+      .stub(user.model, 'create')
+      .resolves(payload);
+    });
+    after(()=>{
+      sinon.restore();
+    });
+    it('a) return status 201 and the user created', async () => {
+      chaiHttpResponse = await chai
+         .request(server.app)
+         .post('/user')
+         .set('X-API-Key', 'foobar')
+         .send({
+          "name": "Roberto",
+          "lastName": "Oliveira",
+          "email": "roberto@email.com",
+          "contact": "11987654321",
+          "password": "123456789",
+          "address": {
+              "street": "avenida",
+              "number": "100A",
+              "district": "Bairro",
+              "zipcode": "45687-899",
+              "city": "cidade",
+              "state": "estado",
+              "country": 'pais'
+          }
+      });
+      expect(chaiHttpResponse).to.have.status(201);
+      expect(chaiHttpResponse.body).to.deep.equal({
+        "_id": "6260bca97c58e5a0b7847cfa",
+        "name": "Roberto",
+        "lastName": "Oliveira",
+        "email": "roberto@email.com",
+        "contact": "11987654321",
+        "password": "123456789",
+        "balance": '',
+        "transactions": [],
+        "address": {
+            "street": "avenida",
+            "number": "100A",
+            "district": "Bairro",
+            "zipcode": "45687-899",
+            "city": "cidade",
+            "state": "estado",
+            "country": 'pais'
+        }
+      });
+    });
+  });
+  describe('1.2 - if fail', () => {
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon
+      .stub(user.model, 'findOne')
+      .rejects({ message: 'Internal Server Error'});
+      sinon
+    });
+    after(()=>{
+      sinon.restore();
+    });
+
+    it('a) return status 500 and the error message "Internal Server Error"', async () => {
+      chaiHttpResponse = await chai
+         .request(server.app)
+         .post('/user')
+         .set('X-API-Key', 'foobar')
+         .send({
+          "name": "Roberto",
+          "lastName": "Oliveira",
+          "email": "roberto@email.com",
+          "contact": "11987654321",
+          "password": "123456789",
+          "address": {
+              "street": "avenida",
+              "number": "100A",
+              "district": "Bairro",
+              "zipcode": "45687-899",
+              "city": "cidade",
+              "state": "estado",
+              "country": 'pais'
+          }
+      });
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body).to.deep.equal({ "error": "Erro: Internal Server Error"});
+    });
+  });
+});

ea40c635f80343a3ae08282406a2f44681e56f10
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-23 00:26:09 -0300

    feat: implemented integration test PUT user/:id

--- project/src/tests/integration/user.test.ts
+++ project/src/tests/integration/user.test.ts
@@ -320,3 +320,104 @@ describe('4 - Test endpoint GET /user', () => {
     });
   });
 });
+
+describe('5 - Test endpoint PUT /user/:id', () => {
+  describe('5.1 - if success', () => {
+    let chaiHttpResponse: Response;
+
+    before(() => {
+      sinon
+      .stub(user.model, 'findOne')
+      .resolves(payload);
+      sinon
+      .stub(user.model, 'findByIdAndUpdate')
+      .resolves(payload);
+    });
+    after(()=>{
+      sinon.restore();
+    });
+
+    it('a) return status 200 and the user updated', async () => {
+      chaiHttpResponse = await chai
+         .request(server.app)
+         .put('/user/6260bca97c58e5a0b7847cfa')
+         .set('authorization', token)
+         .send({
+          "name": "Roberto",
+          "lastName": "Oliveira",
+          "email": "roberto@email.com",
+          "contact": "+5511987654321",
+          "password": "roberto_password",
+          "address": {
+              "street": "avenida",
+              "number": "100A",
+              "district": "Bairro",
+              "zipcode": "45687-899",
+              "city": "cidade",
+              "state": "estado",
+              "country": "pais"
+          }
+      });
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.deep.equal({
+        "_id": "6260bca97c58e5a0b7847cfa",
+        "name": "Roberto",
+        "lastName": "Oliveira",
+        "email": "roberto@email.com",
+        "contact": "+5511987654321",
+        "password": "$2b$10$JOmGDGptDGC1.eLa3OMj0uAk4FxZT2SjLH0lbP3Uh9W7iDHGN3Lp6",
+        "balance": 0,
+        "transactions": [],
+        "address": {
+            "street": "avenida",
+            "number": "100A",
+            "district": "Bairro",
+            "zipcode": "45687-899",
+            "city": "cidade",
+            "state": "estado",
+            "country": "pais"
+        }
+      });
+    });
+  });
+  describe('5.2 - if fail', () => {
+    let chaiHttpResponse: Response;
+    before(() => {
+      sinon
+      .stub(user.model, 'findOne')
+      .resolves(payload);
+      sinon
+      .stub(user.model, 'findByIdAndUpdate')
+      .rejects({ message: 'Internal Server Error'});
+      sinon
+    });
+    after(()=>{
+      sinon.restore();
+    });
+
+    it('a) return status 500 and the error message "Internal Server Error"', async () => {
+      chaiHttpResponse = await chai
+         .request(server.app)
+         .put('/user/6260bca97c58e5a0b7847cfa')
+         .set('authorization', token)
+         .send({
+          "name": "Roberto",
+          "lastName": "Oliveira",
+          "email": "roberto@email.com",
+          "contact": "+5511987654321",
+          "password": "roberto_password",
+          "address": {
+              "street": "avenida",
+              "number": "100A",
+              "district": "Bairro",
+              "zipcode": "45687-899",
+              "city": "cidade",
+              "state": "estado",
+              "country": "pais"
+          }
+      });
+      expect(chaiHttpResponse).to.have.status(500);
+      expect(chaiHttpResponse.body).to.deep.equal({ "error": "Erro: Internal Server Error"});
+    });
+  });
+});
